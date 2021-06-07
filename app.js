bd5053788c27009bb0dadbc58762d33419207e76
gushimaaceline <celinegushimaa@gmail.com>
2021-06-07 23:08:29 +0300

    edited and added few lines of codes

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
- 
-  // Automatic Slideshow - change image every 3 seconds
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000);
+var myObject = {
+  name: "Lovelace",
+  func: function() {
+      var self = this;
+      console.log("outer func:  this.name = " + this.name);
+      console.log("outer func:  self.name = " + self.name);
+
+      (function() {
+          console.log("inner func:  this.name = " + this.name);
+          console.log("inner func:  self.name = " + self.name);
+      }());
   }
+};
+myObject.func();
+//This function will display LoveLace which is the value of func and it is a closure.
+// func is a property which holds a function.All the console statements in func will display lovelace apart from the third one because after
+// reasigning  this to a variable called self, the only way to access name outside the outer function
+//is using self and to access the value of a property of an object we use keyword this
 
-
-  
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function(e){
-    //grab content from  input
-let feet = document.querySelector('#company');
-let inches = document.querySelector('#phone');
-let inche= document.querySelector('#location')
-
-const results = document.querySelector('#results');
-    e.preventDefault();
-
-    company = parseInt(company.value);
-    phone = parseInt(phone.value);
-    location = parseInt(location.value);
-         document.querySelector('#company').value = '';
-        document.querySelector('#phone').value = '';
-        document.querySelector('#location').value = '';
-    });
